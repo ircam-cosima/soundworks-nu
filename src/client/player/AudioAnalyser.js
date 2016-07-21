6c530b7f63588a3f66cbff294b40e8799f3aa21e
David Poirier-Quinot <davipoir@gmail.com>
2016-07-21 18:15:55 +0200

    removed forgotten console.log

--- src/client/player/AudioAnalyser.js
+++ src/client/player/AudioAnalyser.js
@@ -5,9 +5,7 @@ export default class AudioAnalyser {
   constructor() {
 
     this.in = audioContext.createAnalyser();
-    console.log(this.in.smoothingTimeConstant);
     this.in.smoothingTimeConstant = 0.1;
-    console.log(this.in.smoothingTimeConstant);
     this.in.fftSize = 32;
 
     this.freqs = new Uint8Array(this.in.frequencyBinCount);
@@ -26,7 +24,6 @@ export default class AudioAnalyser {
     }
 
     let norm = this.in.frequencyBinCount * 32; // arbitrary value, to be cleaned
-    console.log(amplitude / norm);
     return amplitude / norm;
   }
 
