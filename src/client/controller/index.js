379bad6f1ead3fe728e88eb78b442665bf79423b
David Poirier-Quinot <davipoir@gmail.com>
2017-02-08 17:43:33 +0100

    fixed controller (broken since soundworks update)

--- src/client/controller/index.js
+++ src/client/controller/index.js
@@ -5,24 +5,19 @@ import viewTemplates from '../shared/viewTemplates';
 import viewContent from '../shared/viewContent';
 
 // launch application when document is fully loaded
-const init = () => {
-  // configuration received from the server through the `index.html`
+window.addEventListener('load', () => {
+  // initialize the client with configuration received
+  // from the server through the `index.html`
   // @see {~/src/server/index.js}
   // @see {~/html/default.ejs}
-  const { appName, clientType, socketIO, assetsDomain, standalone, beaconUUID }  = window.soundworksConfig;
-  // initialize the 'player' client
-  soundworks.client.init(clientType, { appName, socketIO });
+  const config = window.soundworksConfig;
+  soundworks.client.init(config.clientType, config);
   soundworks.client.setViewContentDefinitions(viewContent);
   soundworks.client.setViewTemplateDefinitions(viewTemplates);
 
-  // create client side (controller) experience
-  const experience = new ControllerExperience(assetsDomain);
+  // create client side (player) experience
+  const experience = new ControllerExperience(config.assetsDomain);
 
   // start the client
   soundworks.client.start();
-};
-
-if (!!window.cordova)
-  document.addEventListener('deviceready', init);
-else
-  window.addEventListener('load', init);
+});
