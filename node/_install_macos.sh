9008bf2459e4cc4a30bb4ff1ce4406bb3789897f
David Poirier-Quinot <davipoir@gmail.com>
2017-07-03 13:12:30 +0200

    added osx install script

---
+++ node/_install_macos.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# ensure current dir is .
+LIB_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $LIB_DIR
+
+# check for homebrew install
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Suggest to install Homebrew
+    echo 'script aborted: install homebrew to continue (check https://brew.sh/index_fr.html)'
+    exit
+else
+    brew update
+fi
+
+# install node
+brew install node
+
+# install and transpile project
+npm install
+cd node_modules/soundworks && npm run transpile && cd ../.. # to remove once nü no longer on soundworks develop branch
+npm run transpile
+
+# output success
+echo '\n'
+echo '---------------------------'
+echo '## installation complete ##'
+echo '---------------------------'
+echo '\n'
